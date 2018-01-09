d5def73e27d6d2eebc08aeccf92b254417a6b5bd
HenryHall <mnboogie@yahoo.com>
2018-01-09 00:19:24 -0600

    Functionality

--- server/routes/updateMasterList.js
+++ server/routes/updateMasterList.js
@@ -7,7 +7,7 @@ router.put('/', function(req, res){
 
   pg.connect(connection, function(err, client, done){
 
-    client.query('UPDATE spoilermasterlist SET cardname = $1 AND status = "spoiled" WHERE collectornum = $2', [req.body.name, req.body.number])
+    client.query('UPDATE spoilermasterlist SET cardname = $1, status = "spoiled" WHERE collectornum = $2', [req.body.name, req.body.number])
     .then(function(){
       //Success
       var results = [];
